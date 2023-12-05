2513c305aa2df429700475382ef682b9747d35ba
Andrew <Andrey_U_O@mail.ru>
2023-12-05 21:49:53 +0300

    Start Page

---
+++ index.js
@@ -0,0 +1,4 @@
+const redirBtns = document.getElementsByClassName("redirectBtn");
+
+redirBtns[0].addEventListener("click", () => (window.location.href = "#"));
+redirBtns[1].addEventListener("click", () => (window.location.href = "#"));
