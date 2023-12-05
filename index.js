c09f99a8a85ae6a0736ee8a19b6394dc04a47d77
Andrew <Andrey_U_O@mail.ru>
2023-12-05 21:51:52 +0300

    Start Page

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const redirBtns = document.getElementsByClassName("redirectBtn");
 
-redirBtns[0].addEventListener("click", () => (window.location.href = "#"));
-redirBtns[1].addEventListener("click", () => (window.location.href = "#"));
+redirBtns[0].addEventListener(
+  "click",
+  () =>
+    (window.location.href =
+      "https://fumetsunokami.github.io/testReq/employeeTable"),
+);
+redirBtns[1].addEventListener(
+  "click",
+  () =>
+    (window.location.href =
+      "https://fumetsunokami.github.io/testReq/directorsTable"),
+);
